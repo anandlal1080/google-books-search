fbbee6a5ce92c3aeaa7ce06ce185d303853e2c47
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-19 21:03:01 -0700

    book schema

--- models/post.js
+++ models/post.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
+const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  body: String,
-  date: { type: Date, default: Date.now }
+  authors: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
-const Post = mongoose.model("Post", postSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-module.exports = Post;
+module.exports = Book;
